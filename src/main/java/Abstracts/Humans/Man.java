3ed630a4cac559041770617ead1cc83f2c04c002
hunerali <huner1442@gmail.com>
2022-09-01 18:08:10 +0400

    commit

---
+++ src/main/java/Abstracts/Humans/Man.java
@@ -0,0 +1,30 @@
+package Abstracts.Humans;
+
+
+import Abstracts.Enums.DayOfWeeks;
+import Abstracts.Family;
+import Abstracts.Human;
+
+import java.util.Map;
+
+public final class Man extends Human {
+    public void smoking() {
+
+    }
+
+    public Man(String name, String surname, Long year, int iq, Map<DayOfWeeks, String> schedule, Family family) {
+        super(name, surname, year, iq, schedule, family);
+    }
+
+    public Man() {
+    }
+
+    public Man(String name, String surname, Long year) {
+        super(name, surname, year);
+    }
+
+    @Override
+    public void greetPet() {
+        System.out.println("Man greeting pet");
+    }
+}
---
+++ src/main/java/Abstracts/Humans/Women.java
@@ -0,0 +1,31 @@
+package Abstracts.Humans;
+
+import Abstracts.Enums.DayOfWeeks;
+import Abstracts.Family;
+import Abstracts.Human;
+
+import java.util.Map;
+
+
+public final class Women extends Human {
+    public void makeup() {
+
+    }
+
+    public Women(String name, String surname, Long year, int iq, Map<DayOfWeeks, String> schedule, Family family) {
+        super(name, surname, year, iq, schedule, family);
+    }
+
+    public Women() {
+    }
+
+    public Women(String name, String surname, Long year) {
+        super(name, surname, year);
+    }
+
+    @Override
+    public void greetPet() {
+        System.out.println("Women greeting pet");
+    }
+}
+
